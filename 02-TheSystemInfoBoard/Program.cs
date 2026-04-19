ab2d5e0f6790d078b96a8f4a0af01920c5d7a08c
Vusani Matshinge <vmatshinge@icloud.com>
2026-04-19 18:14:13 +0200

    add: system info board display

---
+++ 02-TheSystemInfoBoard/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace TheSystemInfoBoard
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("===============================");
+			Console.WriteLine("   SureGuard Insurance Systems ");
+			Console.WriteLine("===============================");
+			Console.WriteLine("System Status:  Online");
+			Console.WriteLine("Location:       Johannesburg, ZA");
+			Console.WriteLine("Support:        support@sureguard.co.za");
+			Console.WriteLine("===============================");
+		}
+	}
+}
