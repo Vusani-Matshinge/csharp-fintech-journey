5ae2331e060cafc95d0e80c15761b888cfd14c13
Vusani Matshinge <vmatshinge@icloud.com>
2026-04-19 17:58:42 +0200

    first ever commit
    This is the first commit on my journey to perfecting my C# and programming skills

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace TheCompanyGreeting
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Welcome to SureGuard Insurance Systems");
+			Console.WriteLine("System Status: Online");
+			Console.WriteLine("Have a productive day!");
+		}
+	}
+}
