ab205a4960c4cd65ed23a5bfe27428eefabdb447
Vusani Matshinge <vmatshinge@icloud.com>
2026-04-19 18:22:11 +0200

    add: product catalogue menu

---
+++ 03-ProductMenu/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace TheProductMenu
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("=======================================");
+			Console.WriteLine("     SureGuard | Product Catalogue");
+			Console.WriteLine("=======================================");
+			Console.WriteLine("  [1]  Life Insurance");
+			Console.WriteLine("  [2]  Vehicle Insurance");
+			Console.WriteLine("  [3]  Home & Contents Insurance");
+			Console.WriteLine("  [4]  Business Insurance");
+			Console.WriteLine("  [5]  Medical Aid Supplement");
+			Console.WriteLine("=======================================");
+			Console.WriteLine("  Helpline: 0800 123 456  |  24/7");
+			Console.WriteLine("=======================================");
+		}
+	}
+}
