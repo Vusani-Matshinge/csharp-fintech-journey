f92f739f7f80f967bd45d7ea36ff2a5a38a99dbc
Vusani Matshinge <vmatshinge@icloud.com>
2026-04-20 15:00:37 +0200

    add: client profile using variables

---
+++ 04-ClientProfile/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace ClientProfile
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			string dashboardWelcomeMessage = "===== SureGuard Client Profile =====";
+			string divider = "=====================================";
+			string clientName = "Lebo Dlamini";
+			int clientAge = 34;
+			double clientHeight = 1.72;
+			bool active = true;
+
+			Console.WriteLine(dashboardWelcomeMessage);
+			Console.WriteLine($"Name:     {clientName}");
+			Console.WriteLine($"Age:      {clientAge}");
+			Console.WriteLine($"Height:   {clientHeight}");
+			Console.WriteLine($"Active:   {active}");
+			Console.WriteLine(divider);
+
+		}
+	}
+}
